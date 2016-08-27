90c5a6eccaa2d2bdcead919be10ce22097d18241
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-27 14:18:41 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,16 +1,23 @@
 function SomeDirective() {
-	return {
-		template: [
-			'<div>',
-				'Replace this text!',
-			'</div>'
-		].join(''),
-		link: function (scope, elem, attrs) {
-
-		}
+  return {
+    template: [
+      '<div>',
+      'Replace this text!',
+      '</div>'
+    ].join(''),
+    compile: function ($scope, $elem, $attrs) {
+      return { 
+	pre: function (scope, element, attrs) {
+	  // document.getElementByTagName('div')[0].innerHTML('replaced');
+	  element[0].innerHTML('replaced');
+	},
+	post: function (scope, element, attrs) {
+	  element[0].addEventListener('click', function () { console.log('event listener added') });
 	}
+    }
+  }
 }
 
 angular
-	.module('app')
-	.directive('someDirective', SomeDirective);
+  .module('app')
+  .directive('someDirective', SomeDirective);
